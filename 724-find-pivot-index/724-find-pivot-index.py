26cdd5748f1108614ebf8ae22a2e02fddce64bcc
kushal0603 <68853040+kushal0603@users.noreply.github.com>
2022-06-18 11:48:22 +0530

    Time: 171 ms (59.62%), Space: 14.6 MB (44.07%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        S=sum(nums)
+        leftsum=0
+        for i,x in enumerate(nums):
+            if leftsum==(S-leftsum-x):
+                return i
+            leftsum+=x
+        return -1
+        
