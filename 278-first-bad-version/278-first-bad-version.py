9d4c6faf1cbd5cae80f38fa3567d9fb57002b7b3
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-07-12 10:56:41 +0530

    Time: 68 ms (5.67%), Space: 13.8 MB (60.99%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -1,15 +1,12 @@
 class Solution:
-    def firstBadVersion(self, n):
-        result = self.divideSearch((n+1)//2, 1, n)
-        return result
-
-    def divideSearch(self, current, start, end):
-        if isBadVersion(current):
-            if current==start:
-                return start
-            elif not isBadVersion(current-1):
-                return current
+    def firstBadVersion(self, n: int) -> int:
+        high = n
+        low=1
+        
+        while(high >= low ):
+            mid = (high + low) //2
+            if isBadVersion(mid) == True:
+                high = mid-1
             else:
-                return self.divideSearch((start+current)//2, start, current)
-        else:
-            return self.divideSearch((current+end)//2+1, current+1, end)
+                low = mid +1
+        return low
