9159c628a36cf3a5e2f29f859a858208a5466561
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-07-19 21:05:24 +0530

    Time: 50 ms (43.34%), Space: 13.9 MB (66.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,20 +1,3 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows==1:
-            return [[1]]
-        elif numRows==2:
-            return [[1],[1,1]]
-        l=[1,1]
-        l2=[]
-        l3=[]
-        l3.append([1])
-        l3.append(l)
-        for i in range(2,numRows):
-            l2=[]
-            l2.append(1)
-            for j in range(len(l)-1):
-                l2.append(l[j]+l[j+1])
-            l2.append(1)
-            l=l2.copy()
-            l3.append(l2)
-        return l3
+        return [[int(comb(i, j)) for j in range(i+1)] for i in range(numRows)]
