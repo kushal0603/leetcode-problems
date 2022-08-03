79fd11a2eb951449f5037c236f8825c7d49a2a16
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-08-03 23:14:29 +0530

    Time: 89 ms (9.32%), Space: 14.4 MB (18.83%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,5 +1,37 @@
-class Solution(object):
-    def search(self, nums, target):
-        if target in nums:
-            return nums.index(target)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        def find_target(low, high):
+            while low <= high:
+                mid = low + (high - low) // 2
+                if nums[mid] < target:
+                    low = mid + 1
+                elif nums[mid] > target:
+                    high = mid - 1
+                else:
+                    return mid
+            
+            return -1
+        
+        N = len(nums)
+        low = 0
+        high = N - 1
+        
+        # FYI: low should be storing the index of the smallest
+        # element after we complete the while-loop.
+        while low < high:
+            mid = low + (high - low) // 2
+            if nums[mid] > nums[high]:
+                low = mid + 1
+            else:
+                high = mid
+        
+        # verifies whether target exists in the first list
+        target_idx = find_target(0, low - 1)
+        if target_idx != -1: return target_idx
+        
+        # verifies whether target exists in the second list
+        target_idx = find_target(low, N - 1)
+        if target_idx != -1: return target_idx
+        
         return -1
