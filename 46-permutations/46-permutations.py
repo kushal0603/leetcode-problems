ec5fdcb0dc218ea1b4cf971a1b453fa333f267a0
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-08-04 00:32:34 +0530

    Time: 78 ms (19.48%), Space: 14.2 MB (22.42%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,4 +1,17 @@
-from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums))
+        output=[]
+        def recursion(array,path=[]):
+            if len(array)==0:
+                return [path]
+            for i in array:
+                new=array.copy()
+                new.remove(i)
+                cc=recursion(new,path+[i])
+                if cc!=None:
+                    for i in cc:
+                        output.append(i)
+                    
+        
+        recursion(nums)
+        return output
