52089438d15d40c2b386c9347f5ca561a87c6c3a
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-08-03 23:21:42 +0530

    Time: 104 ms (16.52%), Space: 18.1 MB (14.39%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -1,4 +1,17 @@
 class Solution:
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        f = lambda x: [x.val] + f(x.right) + f(x.left) if x else []        
-        return sorted(f(root))[k-1]
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        array = []
+        
+        def helper(array,tree):
+            if tree is None:
+                return
+            helper(array,tree.left)
+            if len(array) == k:
+                return
+            else:
+                array.append(tree.val)
+            helper(array,tree.right)
+        
+        helper(array,root)
+        
+        return array[len(array)-1]
