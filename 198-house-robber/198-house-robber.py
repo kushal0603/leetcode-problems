809994c6f3115152a37e0e6153e04a527644748e
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-08-03 23:24:43 +0530

    Time: 80 ms (5.50%), Space: 13.9 MB (19.58%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,8 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            second = nums[0]
+            first = max(nums[0], nums[1])
+            
+            for i in range(2,len(nums)):
+                current = max(first, second + nums[i])
+                second = first
+                first = current
+                
+            return first
