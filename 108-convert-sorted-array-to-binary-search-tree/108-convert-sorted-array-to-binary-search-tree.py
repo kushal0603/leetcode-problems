7e481d16a97718ec18ec6da6e2d33eb3c20a401e
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-08-10 23:02:56 +0530

    Time: 151 ms (18.40%), Space: 15.7 MB (32.22%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.py
@@ -1,14 +1,11 @@
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:        
-        if len(nums) == 1:
-            return TreeNode(val=nums[0])
-        if len(nums) == 0:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        total_nums = len(nums)
+        if not total_nums:
             return None
 
-        m = len(nums)//2
-
+        mid_node = total_nums // 2
         return TreeNode(
-            val=nums[m],
-            left=self.sortedArrayToBST(nums[:m]),
-            right=self.sortedArrayToBST(nums[m+1:]) # note here is m + 1 because m is already assigned to the parent
+            nums[mid_node], 
+            self.sortedArrayToBST(nums[:mid_node]), self.sortedArrayToBST(nums[mid_node + 1 :])
         )
