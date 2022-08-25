72283db7d830b311497bfd6babd3ed60b175b96c
kushal sai <68853040+kushal0603@users.noreply.github.com>
2022-08-25 18:32:46 +0530

    Time: 79 ms (65.70%), Space: 14.1 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,3 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return Counter(ransomNote) <= Counter(magazine)
+        count = collections.Counter(magazine) # Count frequency of characters in magazine.
+        
+        for char in ransomNote:
+			# If ransomNote has a character which is not present in count 
+			# OR if the count of that character has become 0 , [eg: ransomNote = "aaa" and magazine = "aa"]
+			# then return False
+            if char not in count or count[char] == 0:
+                return False
+            # Decrease counter frequency by 1 at the end of each loop.
+            count[char] -= 1
+            
+        return True
